143bd2012657de33d9278a857bf05762b93d43d2
Muhammad Hamza Zaib <hamza@aurorasolutions.io>
2017-11-19 00:45:01 +0500

    Add support for java.time.period

--- src/main/groovy/org/grails/plugins/web/Jsr310ConvertersConfiguration.groovy
+++ src/main/groovy/org/grails/plugins/web/Jsr310ConvertersConfiguration.groovy
@@ -297,6 +297,38 @@ class Jsr310ConvertersConfiguration {
         }
     }
 
+    @Bean
+    FormattedValueConverter periodConverter() {
+        new FormattedValueConverter() {
+            @Override
+            Object convert(Object value, String format) {
+                Period.parse((CharSequence) value)
+            }
+
+            @Override
+            Class<?> getTargetType() {
+                Period
+            }
+        }
+    }
+
+    @Bean
+    ValueConverter periodValueConverter() {
+        new Jsr310DateValueConverter<Period>() {
+            @Override
+            Period convert(Object value) {
+                convert(value) { String format ->
+                    Period.parse((CharSequence) value)
+                }
+            }
+
+            @Override
+            Class<?> getTargetType() {
+                Period
+            }
+        }
+    }
+
     abstract class Jsr310DateValueConverter<T> implements ValueConverter {
 
         @Override
--- src/test/groovy/org/grails/plugins/web/Jsr310ConvertersConfigurationSpec.groovy
+++ src/test/groovy/org/grails/plugins/web/Jsr310ConvertersConfigurationSpec.groovy
@@ -10,6 +10,7 @@ import java.time.LocalTime
 import java.time.Month
 import java.time.OffsetDateTime
 import java.time.OffsetTime
+import java.time.Period
 import java.time.ZoneId
 import java.time.ZoneOffset
 import java.time.ZonedDateTime
@@ -225,5 +226,23 @@ class Jsr310ConvertersConfigurationSpec extends Specification {
         date.hour == 8
         date.minute == 0
         date.second == 0
+    }
+
+    void "periodConverter"() {
+        given:
+        def converter = config.periodConverter()
+
+        expect:
+        converter.targetType == Period
+        converter.convert("P2D", null) instanceof Period
+    }
+
+    void "periodValueConverter"() {
+        def converter = config.periodValueConverter()
+
+        expect:
+        converter.targetType == Period
+        converter.canConvert('')
+        converter.convert("P2D") instanceof Period
     }
 }
