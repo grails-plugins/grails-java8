7d98290a3233e883d459435fbbaa39eb20d9741e
jameskleeh <james.kleeh@gmail.com>
2017-12-04 22:23:02 -0500

    Fix bson imports

--- src/main/groovy/org/grails/plugins/bson/decoders/InstantDecoder.groovy
+++ src/main/groovy/org/grails/plugins/bson/decoders/InstantDecoder.groovy
@@ -2,7 +2,7 @@ package org.grails.plugins.bson.decoders
 
 import groovy.transform.CompileStatic
 import org.bson.BsonReader
-import org.grails.datastore.bson.codecs.decoders.SimpleDecoder
+import static org.grails.datastore.bson.codecs.decoders.SimpleDecoder.TypeDecoder
 import org.grails.datastore.mapping.engine.EntityAccess
 import org.grails.datastore.mapping.model.PersistentProperty
 import org.grails.plugins.bson.temporal.InstantBsonConverter
@@ -13,7 +13,7 @@ import org.grails.plugins.bson.temporal.InstantBsonConverter
  * @author James Kleeh
  */
 @CompileStatic
-class InstantDecoder implements SimpleDecoder.TypeDecoder, InstantBsonConverter {
+class InstantDecoder implements TypeDecoder, InstantBsonConverter {
 
     @Override
     void decode(BsonReader reader, PersistentProperty property, EntityAccess entityAccess) {
--- src/main/groovy/org/grails/plugins/bson/encoders/InstantEncoder.groovy
+++ src/main/groovy/org/grails/plugins/bson/encoders/InstantEncoder.groovy
@@ -2,7 +2,7 @@ package org.grails.plugins.bson.encoders
 
 import groovy.transform.CompileStatic
 import org.bson.BsonWriter
-import org.grails.datastore.bson.codecs.encoders.SimpleEncoder
+import static org.grails.datastore.bson.codecs.encoders.SimpleEncoder.TypeEncoder
 import org.grails.datastore.mapping.model.PersistentProperty
 import org.grails.plugins.bson.temporal.InstantBsonConverter
 
@@ -14,7 +14,7 @@ import java.time.Instant
  * @author James Kleeh
  */
 @CompileStatic
-class InstantEncoder implements SimpleEncoder.TypeEncoder, InstantBsonConverter {
+class InstantEncoder implements TypeEncoder, InstantBsonConverter {
 
     @Override
     void encode(BsonWriter writer, PersistentProperty property, Object value) {
