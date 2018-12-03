85aa1b29c46351b9f34299fea0ca13eb07acf721
sdelamo <sergio.delamo@softamo.com>
2018-12-03 12:05:46 +0100

    travis openjdk11
    Skip publishing if openjdk11

--- travis-build.sh
+++ travis-build.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 set -e
+
+EXIT_STATUS=0
+
 rm -rf *.zip
-./gradlew clean test assemble
+
+./gradlew clean test assemble || EXIT_STATUS=$?
 
 filename=$(find build/libs -name "*.jar" | head -1)
 filename=$(basename "$filename")
 
-EXIT_STATUS=0
-echo "Publishing archives for branch $TRAVIS_BRANCH"
+if [ $EXIT_STATUS -ne 0 ]; then
+  exit $EXIT_STATUS
+fi
+
+if [ "${TRAVIS_JDK_VERSION}" == "openjdk11" ] ; then
+  exit $EXIT_STATUS
+fi
+
+echo "Publishing archives for branch $TRAVIS_BRANCH JDK: $TRAVIS_JDK_VERSION"
 if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
 
   echo "Publishing archives"
