dcb962e182fc869af35c55776a66a507fc2a2475
sdelamo <sergio.delamo@softamo.com>
2018-12-06 18:05:36 +0100

    Annotate Application with @CompileStatic

--- grails-app/init/grails/jsr310/Application.groovy
+++ grails-app/init/grails/jsr310/Application.groovy
@@ -3,7 +3,9 @@ package grails.jsr310
 import grails.boot.*
 import grails.boot.config.GrailsAutoConfiguration
 import grails.plugins.metadata.*
+import groovy.transform.CompileStatic
 
+@CompileStatic
 @PluginSource
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
